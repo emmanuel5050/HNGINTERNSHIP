5f31739056a9b991d9ab6eb51c61af828684d4e1
mac <mac@macs-MacBook-Pro.local>
2022-10-29 18:38:00 +0100

    HNG9 stage1 task

---
+++ InternDetails/Controllers/BioController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InternDetails.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    public class BioController : ControllerBase
+    {
+        private readonly ILogger<BioController> _logger;
+
+        public BioController(ILogger<BioController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public InternDetails Get()
+        {
+            return new InternDetails
+            {
+                Age = 28,
+                Backend = true,
+                Bio = "I am a fulltime .NET Developer with an Insurance firm. " +
+                "Besides coding, I enjoy listening to music, driving and surfing the internet for new technologies. Cheers!",
+                SlackUsername = "DamoFC"
+            };
+
+        }
+    }
+}
---
+++ InternDetails/InternDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace InternDetails
+{
+    public class InternDetails
+    {
+        public string SlackUsername { get; set; }
+
+        public bool Backend { get; set; }
+
+        public int Age { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
